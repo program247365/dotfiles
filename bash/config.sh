9b82cb963740432693971c0dd5a1e87a494dfb37
Kevin B. Ridgway <kridgway@gmail.com>
2017-12-12 01:11:49 -0500

    Lots of updates.

--- bash/config.sh
+++ bash/config.sh
@@ -1,11 +1,6 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+autoload -U $BASH/functions/*(:t)
 
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
-HISTFILE=~/.zsh_history
+HISTFILE=~/.bash_history
 HISTSIZE=10000
 SAVEHIST=10000
 
@@ -30,10 +25,3 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
