d02bd8e0de67a80f7de6e456b1e8ee5c484d662c
Kevin B. Ridgway <kridgway@gmail.com>
2017-12-14 10:40:40 -0500

    Fix docker aliases.

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -1,3 +1,5 @@
-alias d='docker $*'
-alias dc'docker-compose $*'
+#!/bin/sh
+
+alias d='docker'
+alias dc='docker-compose'
 alias dtail="~/Code/docker-logs-tail/bin/dlt --exclude 'bogart_consul_1'" # assumes you have clone and merged in this PR https://github.com/zendesk/docker-logs-tail/pull/1
