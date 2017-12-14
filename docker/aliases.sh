e1bfe882980897a9972a479889696d7cbfc221b9
Kevin B. Ridgway <kridgway@gmail.com>
2017-12-14 11:03:10 -0500

    Updating cask, changing alias.

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -2,4 +2,4 @@
 
 alias d='docker'
 alias dc='docker-compose'
-alias dtail="~/Code/docker-logs-tail/bin/dlt --exclude 'bogart_consul_1'" # assumes you have clone and merged in this PR https://github.com/zendesk/docker-logs-tail/pull/1
+alias dtail="${HOME}/Code/docker-logs-tail/bin/dlt --exclude 'bogart_consul_1'" # assumes you have clone and merged in this PR https://github.com/zendesk/docker-logs-tail/pull/1
