a71c73b73fb7a738a2ba0c365044940885d13c97
Kevin B. Ridgway <kridgway@gmail.com>
2019-01-16 17:17:01 -0500

    Updated useful aliases and function for url-shortener

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 alias reload!='. ~/.bashrc'
+alias ll="ls -1a"
 alias cls='clear' # Good 'ol Clear Screen command
 alias startday='tiny-care-terminal'
 alias pup='~/go/bin/pup'
+alias npm-update="npx npm-check -u"
+alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
