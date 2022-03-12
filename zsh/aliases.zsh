6b9b1e1b9657ea24eece81139fd7d650c4af9a3e
Kevin B. Ridgway <kridgway@gmail.com>
2022-03-12 14:21:37 -0500

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,5 @@
 alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
-alias ll="ls -1a"
-alias cls='clear' # Good 'ol Clear Screen command
-alias startday='tiny-care-terminal'
-alias pup='~/go/bin/pup'
 alias npm-update="npx npm-check -u"
 alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
+alias ls="lsd -la"
