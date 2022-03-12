ce179d217a48cef86d787d305a2e3795027d9846
Kevin B. Ridgway <kridgway@gmail.com>
2022-03-12 14:30:31 -0500

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias npm-update="npx npm-check -u"
 alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
 alias ls="lsd -la"
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
