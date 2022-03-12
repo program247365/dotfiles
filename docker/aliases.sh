bdd91a7e93ccb6c08d2e8ae2298e0eaab2d3f1c0
Kevin B. Ridgway <kridgway@gmail.com>
2022-03-12 14:36:47 -0500

    Update and rename aliases.sh to aliases.zsh

--- docker/aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-alias d='docker'
-alias dc='docker-compose'
-alias dtail="${HOME}/Code/docker-logs-tail/bin/dlt --exclude 'bogart_consul_1'" # assumes you have clone and merged in this PR https://github.com/zendesk/docker-logs-tail/pull/1
