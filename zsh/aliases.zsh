43fff601fa7f2db28f333171e264f5f18128816c
Kevin Ridgway <k@kbr.sh>
2022-08-16 10:07:24 -0400

    Update ls aliases to exa (https://github.com/ogham/exa)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,8 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias npm-update="npx npm-check -u"
 alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
-alias ls="lsd -la"
+alias ls='exa'
+alias ll='exa -lha'
+alias lr='exa -lha --tree'
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
