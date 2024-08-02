0d3d7f16317d829e7a1fb3a85fcf152f5faf946d
Kevin B. Ridgway <k@kbr.sh>
2024-08-02 14:00:16 -0400

    exa no longer maintained, moving to https://github.com/eza-community/eza.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,9 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias npm-update="npx npm-check -u"
 alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
-alias ls='exa'
-alias ll='exa -lha'
-alias lr='exa -lha --tree'
+alias ls='eza'
+alias ll='eza -lha'
+alias lr='eza -lha --tree'
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias vim="nvim"
